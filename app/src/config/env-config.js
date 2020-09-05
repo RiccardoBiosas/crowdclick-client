942d39ca24447035b39d6d92fcf56e4878af52e8
RiccardoBiosas <rbios@protonmail.com>
2020-09-05 17:30:20 +0200

    :sparkles: Updated dependencies and added React Helmet package. Created component for Matic Widget.

---
+++ app/src/config/env-config.js
@@ -0,0 +1,8 @@
+export const config = {
+    blockchain: {        
+        matic: {
+            maticWidgetID: process.env.REACT_APP_MATIC_WIDGET_ID
+        }
+        
+    }
+}
---
+++ app/src/shared/components/MaticWidget/index.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Helmet } from 'react-helmet'
+
+//Styled
+import StyledGeneralButton from '../../styles/StyledGeneralButton'
+
+const MaticWidget = () => {
+  return (
+    <div>
+      <Helmet>
+        <script
+          src='https://wallet.matic.today/embeds/widget-button.js'
+          data-script-name='matic-embeds'
+        ></script>
+      </Helmet>
+      <StyledGeneralButton
+        buttonWidth='100'
+        buttonColor='blue'
+        buttonTextColor='white'
+        type='button'
+        className='matic-widget-button'
+        data-default-page='deposit'
+        data-wapp-id={process.env.REACT_APP_MATIC_WIDGET_ID}
+      >
+        Deposit
+      </StyledGeneralButton>
+    </div>
+  )
+}
+
+export default MaticWidget
